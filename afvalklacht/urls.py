a16cab952dac921fdffb77cfd193c6a3f9b7fe1c
Paul Wagener <mail@paulwagener.nl>
2014-02-23 14:57:38 +0100

    Klacht pagina dummy formulier

--- afvalklacht/urls.py
+++ afvalklacht/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'klacht.views.home', name='home'),
+    url(r'^bak/(.*)', 'klacht.views.bak', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- klacht/views.py
+++ klacht/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 def home(request):
 
     return render(request, 'home.html')
+
+def bak(request, id):
+    if request.POST:
+        return HttpResponse('Formulier verzonden!')
+
+    return render(request, 'bak.html', {'id': id})
