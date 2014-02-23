0e2d8f5b1dce9c3a262e12e98b71eebf3d023b83
Paul Wagener <mail@paulwagener.nl>
2014-02-23 14:38:48 +0100

    Klacht database

--- klacht/models.py
+++ klacht/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Klacht(models.Model):
+    afvalbak_id = models.IntegerField()
+    datum = models.DateTimeField()
+
+    categorie = models.CharField(max_length=128, choices=(
+        ('vol', 'Vol'),
+        ('defect', 'Werkt niet'),
+        ('anders', 'Anders'),
+    ))
+
+    opmerking = models.CharField(max_length=256)
+
+    status = models.CharField(max_length=128, choices=(
+        ('gemeld', 'Gemeld'),
+        ('onderzocht', 'Wordt onderzocht'),
+        ('ingepland', 'Ingepland'),
+        ('opgelost', 'Opgelost'),
+    ))
+
+    email = models.CharField(max_length=256)
