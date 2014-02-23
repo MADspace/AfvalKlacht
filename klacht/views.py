fbd400e5f41c70719026492fa4e1c72cd8535ab7
Paul Wagener <mail@paulwagener.nl>
2014-02-23 15:28:27 +0100

    Working map

--- klacht/views.py
+++ klacht/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Klacht
+import datetime, csv
+import json
+
 
 def home(request):
+    reader = csv.DictReader(open('afvalbakken.csv'))
+    bakken = json.dumps( [ row for row in reader ] )
 
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'bakken': bakken})
 
 def bak(request, id):
     if request.POST:
+        klacht = Klacht()
+        klacht.afvalbak_id = int(id)
+        klacht.datum = datetime.datetime.now()
+        klacht.categorie = request.POST['categorie']
+        klacht.save()
         return HttpResponse('Formulier verzonden!')
 
     return render(request, 'bak.html', {'id': id})
